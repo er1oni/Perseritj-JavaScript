80f2a981a59326d7a253dfe59f16e7dfc6aa8842
er1oni <erjongashi_@hotmail.com>
2024-10-03 20:17:10 +0300

    ...

---
+++ JS/main.js
@@ -0,0 +1,105 @@
+/*console.log("GoodBye")
+
+var a = 50 +50;
+console.log(a);
+
+var b = 25;
+var c = 30;
+var d = b+c
+console.log(d);
+
+var x =15;
+x+=5;
+console.log(x);
+
+var r = 35;
+r-=5;
+console.log(r);*/
+
+
+/*var v = 5;
+var w = 5;
+console.log(v == w);
+
+var x = 6;
+var y = 5;
+console.log(x != y);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num3 = 4;
+var num4 = 7;
+console.log(num3 < num4);
+
+var num5 = 123;
+var num6 = "123";
+
+console.log(num5 === num6);
+
+var firstName = 'Erjon'
+var lastName = 'Gashi'
+
+var fullName = firstName + " " +lastName;
+
+console.log(fullName);
+
+var school = 'Digital school'
+
+console.log(`Hello from ${school}`)
+
+var age = 24;
+
+console.log(age >= 18 && age <= 56);
+
+var var1 = 5;
+var var2 = 6;
+
+console.log(var1 ==5 || var2 ==5);
+
+var var3 = 9;
+var var4 = 10;
+console.log(! (var3 == var4));
+*/
+
+function showMessage(){
+    alert("THis mesage was inside of a function")
+}
+
+showMessage();
+
+
+/*function sum(num1, num2){
+    return num1+num2
+}
+
+console.log(sum(25, 35));
+
+
+var arrowfunction = () => alert('Hello World');
+
+arrowfunction();
+
+
+var Greeting = name => alert(`Hello ${name}`);
+Greeting("Erjon Gashi");
+
+*/
+
+function dsFunction(){
+    var localvar ="Digital School";
+    alert (localvar);
+}
+
+dsFunction()
+
+// alert (localVar);
+
+
+function toSeconds(mintes){
+    return minutes * 60;
+}
+
+console.log (toSsconds(60));
+
